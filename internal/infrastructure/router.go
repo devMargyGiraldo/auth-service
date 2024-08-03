064a7ca47aa98cf7effdf912633dbd4ae8665886
Margy Giraldo <margygiraldo@icloud.com>
2024-08-03 14:29:35 -0500

    Add OpenApi3.1

--- internal/infrastructure/router.go
+++ internal/infrastructure/router.go
@@ -2,6 +2,7 @@ package infrastructure
 
 import (
 	"auth_service/internal/interfaces/handler"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -17,5 +18,9 @@ func NewRouter(authHandler *handler.AuthHandler) *mux.Router {
 }
 
 func StartServer(r *mux.Router) {
-	http.ListenAndServe(":8080", r)
+	log.Println("Server starting on port 8080...")
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
 }
